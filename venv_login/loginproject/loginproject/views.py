763c9cb338de45ad38bc3fbda791198b93c8fe3c
matajupi <kousukehutamata@gmail.com>
2020-11-03 00:28:41 +0900

    login

---
+++ venv_login/loginproject/loginproject/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+class HomeView(TemplateView):
+    template_name = 'home.html'
